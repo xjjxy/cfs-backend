204b0fe871fd2eac53fa76673076a1861107bcb0
xjjxy <1791384179@qq.com>
2020-04-28 09:50:15 +0800

    文件上传出大问题

--- cfs/src/main/java/com/hutb/cfs/donor/dao/BasicDonorDao.java
+++ cfs/src/main/java/com/hutb/cfs/donor/dao/BasicDonorDao.java
@@ -3,6 +3,7 @@ package com.hutb.cfs.donor.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.hutb.cfs.basedao.BaseDao;
 import com.hutb.cfs.donor.entity.Donor;
@@ -14,4 +15,7 @@ public interface BasicDonorDao extends BaseDao {
 	
 	@Select("select * from t_cfs_donor_user_info where username = #{username} and password = #{password}")
 	Donor getDonor(@Param("username")String username,@Param("password")String password);
+	
+	@Update("update t_cfs_donor_user_info set uuid = #{uuid} where id = #{id}")
+	int addWalletUUID(@Param("uuid")String uuid,@Param("id")int id);
 }
